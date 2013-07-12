ce1a65cdf4362f5acef5a094ee1384ab58aa552e
Julian Garcia <julian.garcia@pobox.com>
2013-07-12 14:41:20 +0200

    Mock file for unit testing

---
+++ tests.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 31, 2013
+
+@author: garcia
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def testMock(self):
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
