a3779cf6bfa4a3cc4a582bbde44e5f969495632c
Julian Garcia <julian.garcia@pobox.com>
2013-07-17 11:33:26 +0200

    removed video functions and organized parameters for generality

--- paramless.py
+++ paramless.py
@@ -1,13 +1,11 @@
-import matplotlib.animation as animation
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def one_norm_distance(u, v):
     return np.sum(np.abs(u - v))
 
 
-def mutate(vector, epsilon):
+def point_mutation(vector, epsilon):
     mutant = np.copy(vector)
     position = np.random.randint(len(vector))
     if (np.random.randint(2)):
@@ -21,8 +19,8 @@ def target_function(x):
     return x ** 2.0
 
 
-def evolution_step(resident_surface, target_surface, distance_function=one_norm_distance, **kwargs):
-    mutant = mutate(resident_surface, **kwargs)
+def evolution_step(resident_surface, target_surface, mutation_function=point_mutation, distance_function=one_norm_distance, **kwargs):
+    mutant = mutation_function(resident_surface, **kwargs)
     distance_resident = distance_function(resident_surface, target_surface)
     distance_mutant = distance_function(mutant, target_surface)
     if distance_mutant < distance_resident:
@@ -30,28 +28,9 @@ def evolution_step(resident_surface, target_surface, distance_function=one_norm_
     return resident_surface
 
 
-def update(frameid, x_evolving, number_of_generations, line, x, target):
-    x_evolving = evolution_step(x_evolving, number_of_generations,
-                        target_surface=target, distance_function=one_norm_distance)
-    #print x_evolving, frameid
-    line.set_xdata(x)
-    line.set_ydata(x_evolving)
-
-
 def main():
     # initialize
-    number_of_generations = 1
-    video_name = 'evolution.m4v'
-    x = np.linspace(-1.0, 1.0, 10)  
-    target = target_function(x)
-    x_evolving = np.zeros_like(x)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(x, target_function(x))
-    line, = ax.plot(x, target)
-    ani = animation.FuncAnimation(
-        fig, update, fargs=[x_evolving, number_of_generations, line, x, target], interval=200)
-    ani.save(video_name, writer=animation.FFMpegFileWriter())
+    pass
 
 if __name__ == '__main__':
     #main()
