fe0f2d8dbd2b8da7802fe85217e3fc59d4b462e4
Julian Garcia <julian.garcia@pobox.com>
2013-07-19 12:37:26 +0200

    added epsilon to fitness comparison, and renamed the mutation epsilon to avoid confusion

--- paramless.py
+++ paramless.py
@@ -4,16 +4,21 @@ import matplotlib.animation as animation
 from collections import OrderedDict as OrderedDict
 from copy import deepcopy as deepcopy
 
-def point_mutation(vector, epsilon, **kwargs):
+
+# default tolerance for float comparisons
+DEFAULT_ATOL = 1e-3
+
+
+def point_mutation(vector, mutation_epsilon, **kwargs):
     """
     This is the simplest mutation function, it hits one single position of the vector and pushes it up (or down) by epsilon.
     """
     mutant = np.copy(vector)
     position = np.random.randint(len(vector))
     if (np.random.randint(2)):
-        mutant[position] = mutant[position] + epsilon
+        mutant[position] = mutant[position] + mutation_epsilon
     else:
-        mutant[position] = mutant[position] - epsilon
+        mutant[position] = mutant[position] - mutation_epsilon
     return mutant
 
 
@@ -25,20 +30,13 @@ def one_norm_distance(u, v):
     return np.sum(np.abs(u - v))
 
 
-def target_function(x):
-    """
-    One target function for testing
-    """
-    return x ** 2.0
-
-
 def distance_fitness_function(resident, mutant, target_surface, **kwargs):
     fitness_resident = 1.0 / one_norm_distance(resident, target_surface)
     fitness_mutant = 1.0 / one_norm_distance(mutant, target_surface)
     return fitness_resident, fitness_mutant
 
 
-def evolution_step(resident_surface, fitness_function, mutation_function, **kwargs):
+def evolution_step(resident_surface, fitness_function, mutation_function, atol, **kwargs):
     """
     One generation iteration
     """
@@ -46,13 +44,13 @@ def evolution_step(resident_surface, fitness_function, mutation_function, **kwar
     mutant = mutation_function(resident_surface, **kwargs)
     [fitness_resident, fitness_mutant] = fitness_function(
         resident_surface, mutant, **kwargs)
-    if fitness_resident < fitness_mutant:
+    if fitness_resident < fitness_mutant and abs(fitness_resident-fitness_mutant) > atol:
         resident_surface = np.copy(mutant)
         invasion = True
     return resident_surface, invasion
 
 
-def evolve(initial_surface, fitness_function, mutation_function, iterations, return_time_series=False, seed=None, **kwargs):
+def evolve(initial_surface, fitness_function, mutation_function, iterations, atol=DEFAULT_ATOL, return_time_series=False, seed=None, **kwargs):
     """
     Evolve to a target
     Returns a dict full of information, plus time series data if required
@@ -69,9 +67,10 @@ def evolve(initial_surface, fitness_function, mutation_function, iterations, ret
         if return_time_series:
             previous_resident = np.copy(resident)
         resident, invasion = evolution_step(
-            resident, fitness_function, mutation_function, **kwargs)
+            resident, fitness_function, mutation_function, atol, **kwargs)
         if (return_time_series and invasion):
-            time_series[seq] = {"alive": step - last_entry_time, "resident": previous_resident}
+            time_series[seq] = {
+                "alive": step - last_entry_time, "resident": previous_resident}
             last_entry_time = step
             seq += 1
     if return_time_series:
@@ -86,7 +85,7 @@ def _number_of_generations_in_summary_dict(summary_ordered_dict):
     Counts the number of generations by summing up all the alive times
     """
     suma = 0
-    for i in xrange(min(summary_ordered_dict.keys()), max(summary_ordered_dict.keys())+1):
+    for i in xrange(min(summary_ordered_dict.keys()), max(summary_ordered_dict.keys()) + 1):
         suma += summary_ordered_dict[i]['alive']
     return suma
 
@@ -122,7 +121,7 @@ def create_video_from_time_series(series_compact, target_surface, domain, filena
     Creates a video from a time series dict
     """
     # preliminaries
-    #preprocess series
+    # preprocess series
     time_series = _expand_compact_time_series(series_compact, record_every)
     # define a writer
     Writer = animation.writers['ffmpeg']
