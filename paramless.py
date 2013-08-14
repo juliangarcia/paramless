edd689db2f338783f537783976cb9b45ede662d2
Julian Garcia <julian.garcia@pobox.com>
2013-08-14 16:19:00 +0200

    added a within_range function for bounded mutations

--- paramless.py
+++ paramless.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2013-08-14 12:59
+@summary: Paramless evolution on a line
+@author: garcia
+'''
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,28 +14,53 @@ from copy import deepcopy as deepcopy
 # default tolerance for float comparisons
 DEFAULT_ATOL = 1e-8
 
+def within_bounds(vector, lower_bound=None, upper_bound=None):
+    '''
+    @summary: Determines if the values of vector are in the [lower_bound, upper_bound] interval
+    @param vector: surface to test
+    @param lower_bound: lower_bound
+    @param upper_bound: upper_bound
+    @result: True or False 
+    @todo: Add tolerance!
+    '''
+    if lower_bound != None and np.min(vector) < lower_bound :
+        return False
+    if upper_bound != None and np.max(vector) > upper_bound:
+        return False
+    return True
 
-def point_mutation(vector, mutation_epsilon, force_positive=False, **kwargs):
-    """
-    This is the simplest mutation function, it hits one single position of the vector and pushes it up (or down) by epsilon.
-    """
+
+def point_mutation(vector, mutation_epsilon, lower_bound=None, upper_bound=None):
+    '''
+    @summary: This is the simplest mutation function, it hits one single position of the vector and pushes it up (or down) by epsilon.
+    If lower_bound and upper_bound are not None, it keeps the mutant bounded by only allowing in mutations that do not take things 
+    out of bounds.
+    @param vector: Parent surface to be mutated
+    @param mutation_epsilon: epsilon of the perturbation
+    @param lower_bound: default None, lower bound of the codomain if necessary
+    @param upper_bound: default None, upper bound of the codomain if necessary
+    @result: New mutant surface
+    '''
     mutant = np.copy(vector)
     position = np.random.randint(len(vector))
     if (np.random.randint(2)):
         mutant[position] = mutant[position] + mutation_epsilon
     else:
         mutant[position] = mutant[position] - mutation_epsilon
-    if force_positive and mutant[position] < 0.0:
-        mutant[position] = 0.0
+    if lower_bound is not None and mutant[position] < lower_bound:
+        mutant[position] = lower_bound
+    if upper_bound is not None and mutant[position] > upper_bound:
+        mutant[position] = upper_bound
     return mutant
 
+
 def point_mutation_distribution(vector, mutation_epsilon, **kwargs):
-    #moves up a random point, and down a random point at the same time
+    # moves up a random point, and down a random point at the same time
     mutant = np.copy(vector)
     (position_up, position_down) = np.random.randint(len(vector), size=2)
     adjusted_epsilon = mutation_epsilon
-    #taking care that no negative values are allowed
-    if (mutant[position_down]-mutation_epsilon) <0:
+    # taking care that no negative values are allowed
+    if (mutant[position_down] - mutation_epsilon) < 0:
         adjusted_epsilon = mutant[position_down]
     mutant[position_up] = mutant[position_up] + adjusted_epsilon
     mutant[position_down] = mutant[position_down] - adjusted_epsilon
@@ -37,39 +68,51 @@ def point_mutation_distribution(vector, mutation_epsilon, **kwargs):
 
 
 def _gaussian_mutation_helper(x, mutation_epsilon, loc, width):
-    return mutation_epsilon*(math.e**-(((x-loc)**2.0)/width))
+    return mutation_epsilon * (math.e ** -(((x - loc) ** 2.0) / width))
 
-def gaussian_mutation(vector, mutation_epsilon, domain, width, force_positive=False, **kwargs):
+
+def gaussian_mutation(vector, mutation_epsilon, domain, width, lower_bound=None, upper_bound=None, **kwargs):
     location_index = np.random.randint(0, len(vector))
     location_value = domain[location_index]
-    mutant=np.copy(vector) 
+    mutant = np.copy(vector)
+    function_value_at_location = vector[location_index]
+    # upwards
     if (np.random.randint(2)):
-        perturbation = _gaussian_mutation_helper(domain, mutation_epsilon=mutation_epsilon, loc=location_value, width=np.random.rand()*width)
-        mutant+=perturbation
+        adjusted_epsilon = mutation_epsilon
+        if function_value_at_location + mutation_epsilon > upper_bound:
+            adjusted_epsilon = upper_bound - function_value_at_location
+        perturbation = _gaussian_mutation_helper(
+            domain, mutation_epsilon=adjusted_epsilon, loc=location_value, width=np.random.rand() * width)
+        mutant += perturbation
+    # downwards
     else:
         adjusted_epsilon = mutation_epsilon
-        if force_positive:
-            adjusted_epsilon = np.min(vector)
-        perturbation = _gaussian_mutation_helper(domain, mutation_epsilon=adjusted_epsilon, loc=location_value, width=np.random.rand()*width)
-        mutant-=perturbation
+        if function_value_at_location - mutation_epsilon < lower_bound:
+            adjusted_epsilon = function_value_at_location - lower_bound
+        perturbation = _gaussian_mutation_helper(
+            domain, mutation_epsilon=adjusted_epsilon, loc=location_value, width=np.random.rand() * width)
+        mutant -= perturbation
     return mutant
 
 
 def gaussian_mutation_distribution(vector, mutation_epsilon, domain, width, **kwargs):
-    (location_index_up, location_index_down) = np.random.randint(len(vector), size=2)
+    (location_index_up, location_index_down) = np.random.randint(
+        len(vector), size=2)
     location_value_up = domain[location_index_up]
     location_value_down = domain[location_index_down]
-    mutant=np.copy(vector) 
+    mutant = np.copy(vector)
     minimum_value = np.min(vector)
-    if minimum_value - mutation_epsilon <0:
-        adjusted_epsilon=minimum_value
+    if minimum_value - mutation_epsilon < 0:
+        adjusted_epsilon = minimum_value
     else:
         adjusted_epsilon = mutation_epsilon
-    width = np.random.rand()*width
-    perturbation_up = _gaussian_mutation_helper(domain, mutation_epsilon=adjusted_epsilon, loc=location_value_up, width=width)
-    perturbation_down = _gaussian_mutation_helper(domain, mutation_epsilon=adjusted_epsilon, loc=location_value_down, width=width)
-    mutant+=perturbation_up
-    mutant-=perturbation_down
+    width = np.random.rand() * width
+    perturbation_up = _gaussian_mutation_helper(
+        domain, mutation_epsilon=adjusted_epsilon, loc=location_value_up, width=width)
+    perturbation_down = _gaussian_mutation_helper(
+        domain, mutation_epsilon=adjusted_epsilon, loc=location_value_down, width=width)
+    mutant += perturbation_up
+    mutant -= perturbation_down
     return mutant
 
 
@@ -218,9 +261,8 @@ def create_video_from_time_series(series_compact, target_surface, domain, filena
 
 
 def main():
-    # initialize
     pass
+        
 
 if __name__ == '__main__':
-    # main()
-    pass
+    main()
--- tests.py
+++ tests.py
@@ -4,12 +4,26 @@ Created on Jan 31, 2013
 @author: garcia
 '''
 import unittest
-
+import numpy as np
+from paramless import within_bounds
 
 class Test(unittest.TestCase):
 
-    def testMock(self):
-        self.assertEqual(1,1)
+    # def testPointMutationWithBounds(self):
+    #     x = np.linspace(0.01, 1.0, 100, endpoint=True)
+    #     parent = np.zeros_like(x)
+    #     self.assertTrue(np.max(point_mutation(parent, mutation_epsilon= 0.8, lower_bound=0.0, upper_bound=0.5))<= 0.5)
+    #     self.assertTrue(np.min(point_mutation(parent, mutation_epsilon= 0.8, lower_bound=0.0, upper_bound=0.5))>= 0.0)
 
+
+    def testWithinBounds(self):
+        zeroes_vector = np.zeros(10)
+        self.assertTrue(within_bounds(zeroes_vector, lower_bound=-1.0, upper_bound=1.0), "test 1")
+        self.assertTrue(within_bounds(zeroes_vector, lower_bound=0.0, upper_bound=1.0), "test 2")
+        ones_vector = np.ones(10)
+        self.assertTrue(within_bounds(ones_vector, lower_bound=0.0, upper_bound=1.0), "test 3")
+        self.assertFalse(within_bounds(ones_vector, lower_bound=0.0, upper_bound=0.5), "test 4")
+        
+        
 if __name__ == "__main__":
     unittest.main()
